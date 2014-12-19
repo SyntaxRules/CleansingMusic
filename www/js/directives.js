eb0ab2d359db4f7b81c6ee534a455f0cb1644ce2
syntaxrules <syntaxrulesdevelopment@gmail.com>
2014-12-19 00:19:55 -0800

    Fix up home page to look nice. Add a sweet footer bar.

--- www/js/directives.js
+++ www/js/directives.js
@@ -7,4 +7,28 @@ angular.module('cleansingMusic')
                 $(element).fitText();
             }
         };
-    });
+    }).directive('footerBar', function(MusicService) {
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/footerBar.html',
+            link: function (scope, ele, attrs) {
+                var init = function() {
+                    scope.station = MusicService.getPlayingStation();
+                };
+
+                scope.getStationTitle = function() {
+                    return MusicService.getPlayingStation().title;
+                };
+
+                scope.stop = function () {
+                    MusicService.stop();
+                };
+
+                scope.isPlaying = function () {
+                    return MusicService.isPlaying();
+                };
+
+                init();
+            }
+        }
+    })
