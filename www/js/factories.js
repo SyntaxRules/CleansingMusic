2d8237ffcf9df5afba9c369e935cda57596f72ae
syntaxrules <syntaxrulesdevelopment@gmail.com>
2014-12-18 18:23:13 -0800

    add correct stream urls

--- www/js/factories.js
+++ www/js/factories.js
@@ -56,18 +56,18 @@ angular.module('cleansingMusic')
 })
     .factory('PlaylistService', function() {
          var playlists = [
-            { title: '50\'s Bop', id: 1, streamingUrl: 'http://74.124.12.136:8014/' },
-            { title: '60\'s Pop', id: 2, streamingUrl: 'http://74.124.12.136:8014/' },
-            { title: '70\'s Rock', id: 3, streamingUrl: 'http://74.124.12.136:8014/' },
-            { title: '80\'s Thrill', id: 4, streamingUrl: 'http://74.124.12.136:8014/' },
-            { title: '90\'s Cill', id: 5, streamingUrl: 'http://74.124.12.136:8014/' },
-            { title: '2000\'s Mil', id: 6, streamingUrl: 'http://74.124.12.136:8014/' },
-            { title: 'Classically', id: 7, streamingUrl: 'http://74.124.12.136:8014/' },
-            { title: 'Inspirational', id: 8, streamingUrl: 'http://74.124.12.136:8014/' },
-            { title: 'Beyond Words', id: 9, streamingUrl: 'http://74.124.12.136:8014/' },
-            { title: 'Country Comfort', id: 10, streamingUrl: 'http://74.124.12.136:8014/' },
-            { title: 'Off Track', id: 11, streamingUrl: 'http://74.124.12.136:8014/' },
-            { title: 'Mixup', id: 12, streamingUrl: 'http://74.124.12.136:8014/' },
+            { title: '50\'s Bop', id: 1, streamingUrl: 'http://74.124.12.136:8000/' },
+            { title: '60\'s Pop', id: 2, streamingUrl: 'http://74.124.12.136:8002/' },
+            { title: '70\'s Rock', id: 3, streamingUrl: 'http://74.124.12.136:8004/' },
+            { title: '80\'s Thrill', id: 4, streamingUrl: 'http://74.124.12.136:8006/' },
+            { title: '90\'s Chill', id: 5, streamingUrl: 'http://74.124.12.136:8008/' },
+            { title: '2000\'s Mil', id: 6, streamingUrl: 'http://74.124.12.136:8010/' },
+            { title: 'Classically', id: 7, streamingUrl: 'http://74.124.12.136:8016/' },
+            { title: 'Inspirational', id: 8, streamingUrl: 'http://74.124.12.136:8020/' },
+            { title: 'Beyond Words', id: 9, streamingUrl: 'http://74.124.12.136:8012/' },
+            { title: 'Country Comfort', id: 10, streamingUrl: 'http://74.124.12.136:8018/' },
+            { title: 'Off Track', id: 11, streamingUrl: 'http://74.124.12.136:8024/' },
+            { title: 'Mixup', id: 12, streamingUrl: 'http://74.124.12.136:8022/' },
             { title: 'Christmas', id: 13, streamingUrl: 'http://74.124.12.136:8014/' }
         ];
         var service = {
